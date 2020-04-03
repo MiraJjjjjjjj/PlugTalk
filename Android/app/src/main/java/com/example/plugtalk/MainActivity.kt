9615457c09ad17e1e455199a508b139b6e41d103
Mira <mira.srbn@>
2020-04-03 16:17:07 +0300

    Added registration and login activities

--- Android/app/src/main/java/com/example/plugtalk/MainActivity.kt
+++ Android/app/src/main/java/com/example/plugtalk/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.plugtalk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity", "Email is: $email")
+            Log.d("MainActivity", "Password: $password")
+        }
+
+        already_have_account_view_register.setOnClickListener{
+            Log.d("MainActivity", "Try to show login activity")
+
+            // launch the login activity somehow
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
